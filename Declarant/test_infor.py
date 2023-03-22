4fed666a27610cc6badb44312f634196e78078ca
xh791539879 <791539879@qq.com>
2023-03-22 17:09:33 +0800

    封装selenium

--- Declarant/test_infor.py
+++ Declarant/test_infor.py
@@ -1,5 +1,6 @@
-
 from time import sleep
+
+from selenium.webdriver.support.select import Select
 from webdriver_helper import *
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -10,12 +11,33 @@ class TestInfor:
         chrome_options = Options()
         chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9527")
         driver = get_webdriver(options=chrome_options)
-        driver.get('http://192.168.0.40:18700/#/login')
+        driver.get('http://192.168.0.40:18400/index#/login')
         sleep(1)
-        driver.find_element(by=By.ID,value='name').send_keys('130203202303016830')
-        driver.find_element(by=By.ID,value='password').send_keys('123456')
+        driver.find_element(By.ID, 'name').send_keys('hnssgw')
+        driver.find_element(By.ID, 'password').send_keys('123456')
         sleep(1)
-        driver.find_element(by=By.TAG_NAME,value='button').click()
+        driver.find_element(By.XPATH, "//button[@class='ant-btn ant-btn-primary login-btn ant-btn-lg']").click()
         sleep(5)
-        msg = driver.find_element(by=By.CLASS_NAME,value='title').text
-        assert msg =="课题申报"
+        driver.find_element(By.XPATH, "//li[2]//div[1]").click()
+        sleep(1)
+        driver.find_element(By.LINK_TEXT, "履职考核").click()
+
+    def test_delete(self):
+        chrome_options = Options()
+        chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9527")
+        driver = get_webdriver(options=chrome_options)
+        driver.get('http://192.168.0.40:18400/index#/login')
+        sleep(1)
+        driver.find_element(By.ID, 'name').send_keys('hnssgw')
+        driver.find_element(By.ID, 'password').send_keys('123456')
+        sleep(1)
+        driver.find_element(By.XPATH, "//button[@class='ant-btn ant-btn-primary login-btn ant-btn-lg']").click()
+        sleep(5)
+        driver.find_element(By.XPATH, "//li[2]//div[1]").click()
+        sleep(1)
+        driver.find_element(By.LINK_TEXT, "履职考核").click()
+        sleep(1)
+        driver.find_elements(By.LINK_TEXT, "删除")[0].click()
+        sleep(1)
+        driver.find_element(By.XPATH,
+                            "//div[@class='ant-modal-confirm-btns']//button[@class='ant-btn ant-btn-danger']").click()
---
+++ base/base_page.py
@@ -0,0 +1,26 @@
+
+"""
+封装selenium中的方法
+"""
+
+from webdriver_helper import *
+from selenium.webdriver.chrome.options import Options
+
+class base:
+    def get_url(self):
+        chrome_options = Options()
+        chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9527")
+        self.driver = get_webdriver(options=chrome_options)
+        self.driver.implicitly_wait(5)  # 隐性等待
+
+    def get(self, url):
+        self.driver.get(url)
+
+    def locator_element(self, args):
+        return self.driver.find_element(*args)  # 将By. name和value看成一个元组
+
+    def send_keys(self, args, value):
+        self.locator_element(args).send_keys(value)
+
+    def click(self, args):
+        self.locator_element(args).click()
